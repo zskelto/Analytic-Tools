776176d993dcb22e75d2b537bc0b0b17bc17147d
Zachary Skelton <zskelto@g.clemson.edu>
2020-01-31 11:55:25 -0500

    modified:   virustotal.py

--- lib/virustotal.py
+++ lib/virustotal.py
@@ -7,12 +7,25 @@ class VirusTotal:
 
     def file_report(self, md5):
         file_report_url = self.url + "file/report"
-        PARAMS = {'apikey' : self.apikey, 'resource' : md5}
-        response = requests.get(url = file_report_url, params = PARAMS)
+        params = {'apikey' : self.apikey, 'resource' : md5}
+        response = requests.get(url = file_report_url, params = params)
         return response.json()
 
     def file_scan(self, file_location):
         file_scan_url = self.url + "file/scan"
-        data = {'apikey' : self.apikey, 'file' : file_location}
-        response = requests.post(url = file_scan_url, data = data)
+        files = {'file' : (file_location, open(file_location,'rb'))}
+        params = {'apikey' : self.apikey}
+        response = requests.post(url = file_scan_url, files=files, params = params)
+        return response.json()
+    
+    def url_report(self, url):
+        url_report_url = self.url + "url/report"
+        params = {'apikey' : self.apikey, 'resource' : url}
+        response = requests.get(url = url_report_url, params = params)
+        return response.json()
+
+    def url_scan(self, url):
+        url_scan_url = self.url + "url/scan"
+        params = {'apiley' : self.apikey, 'url' : url}
+        response = requests.post(url=url_scan_url, params = params)
         return response.json()

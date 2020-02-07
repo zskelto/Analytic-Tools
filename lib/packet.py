8545d33e8f2c3952e8d43ff01d55b658c9e6abbb
zskelto <zskelto@g.clemson.edu>
2020-02-07 12:27:24 -0500

    Added tcp parsing

--- lib/packet.py
+++ lib/packet.py
@@ -3,7 +3,7 @@ import socket, sys, struct
 def collect_packets():
     s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
     i = 0
-    while i < 5:
+    while i < 20:
         packet = s.recvfrom(65565)
         packet = packet[0]
         parse(packet)
@@ -17,6 +17,8 @@ def parse(packet):
     data['eth'] = parse_eth(packet)
     if(data['eth']['protocol'] == 8):
         data['ip'] = parse_ip(packet)
+        if(data['ip']['protocol'] == 6):
+            data['tcp'] = parse_tcp(packet,data['ip']['length'])
     print(data)
 
 def parse_eth(packet):
@@ -36,8 +38,14 @@ def parse_ip(packet):
     dest_ip = socket.inet_ntoa(ip[9])
     return {'version':version, 'length':ip_length, 'ttl':ttl, 'protocol':protocol, 'src':src_ip, 'dst':dest_ip}
 
-def parse_tcp(packet):
-    return
+def parse_tcp(packet,ip_length):
+    tcp = struct.unpack('!HHLLBBHHH',packet[14+ip_length:14+ip_length+20])
+    src_port = tcp[0]
+    dst_port = tcp[1]
+    tcp_length = (tcp[4] >> 4) * 4
+    flags = tcp[5]
+    data = packet[14+ip_length+tcp_length:]
+    return {'src_port':src_port, 'dst_port':tcp[1], 'length':tcp_length,'flag':flags,'data':data}
 
 def parse_udp(packet):
     return

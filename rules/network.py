79a8b11894ebf22a0a5acd9b6b8088802542da51
zskelto <zskelto@g.clemson.edu>
2020-02-21 13:20:35 -0500

    modified:   rules/network.py

--- rules/network.py
+++ rules/network.py
@@ -7,11 +7,23 @@ class Network():
         self.connections = {}
 
     def check_tcp(self,packet):
-        if(self.ip != packet['ip']['src']):
+        if(self.mac != packet['eth']['src_mac']):
             ip = packet['ip']['src']
-            self.connections[ip] = {}
         else:
-            self.connections[ip] = {}
+            ip = packet['ip']['dst']
+        
+        if not(ip in self.connections):
+            self.connections[ip] = {'SYN-Count':0,'Last-Flag':0}
+        
+        if(packet['ip']['src'] == ip):
+            #Recieved SYN
+            if(packet['tcp']['flag'] == 2):
+                self.connections[ip]['SYN-Count'] += 1
+                self.connections[ip]['Last-Flag'] = 2
+            #Recieved ACK
+            elif(packet['tcp']['flag'] == 16):
+                if(self.connections[ip]['Last-Flag'] == 2):
+                    self.connections[ip]['SYN-Count'] -= 1
         return 
 
     def check_udp(packet):
